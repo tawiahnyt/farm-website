b4bc4513d64aef525a4413bc1b565ac99bf34dce
tawiah <tawiahin4k@gmail.com>
2024-12-27 08:34:14 +0000

    update Home page layout; adjust text sizes and add values section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,10 +11,10 @@ const Home = () => {
         <div className="row h-[700px]">
           <div className="mt-[-580px] text-center justify-center">
             <div className="lg:px-[25%] md:px-10">
-              <h2 className="lg:text-5xl md:text-3xl text-xl font-bold">
+              <h2 className="lg:text-5xl md:text-3xl text-2xl font-bold">
                 XYZ FOODS LIMITED
               </h2>
-              <p className="p-4 text-xl">
+              <p className="p-4 md:text-xl text-md">
                 XYZ Food Ltd. Is an international company operating in West and
                 Central Africa focusing on improving quality of life and
                 well-being through improvement in agriculture.
@@ -27,15 +27,36 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="flex">
-        <div className="row">
-          <div className="lg:col-span-5 sm:col-span-12 hidden lg:block">
-            <div>
-              <img src={sectionImg} alt="section" width={"550px"} />
-            </div>
-          </div>
-          <div className="lg:col-span-7 sm:col-span-12">
+      <div className="flex px-[15%]">
+        <img
+          src={sectionImg}
+          alt="section"
+          width={"550px"}
+          className="hidden lg:block"
+        />
+        <div className="w-full px-5">
+          <div className="justify-center text-center">
             <h2>Why Choose XYZ Foods Ltd?</h2>
+            <h3>Our Values</h3>
+          </div>
+
+          <div className="grid md:grid-cols-2 sm:grid-cols-12">
+            <div className="p-3">
+              <h3>Text</h3>
+              <p>Paragraph!</p>
+            </div>
+            <div className="p-3">
+              <h3>Text</h3>
+              <p>Paragraph!</p>
+            </div>
+            <div className="p-3">
+              <h3>Text</h3>
+              <p>Paragraph!</p>
+            </div>
+            <div className="p-3">
+              <h3>Text</h3>
+              <p>Paragraph!</p>
+            </div>
           </div>
         </div>
       </div>
