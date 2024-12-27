f8f39541983c8a54ff2d52c34ec2b440b27692a1
tawiah <tawiahin4k@gmail.com>
2024-12-27 16:05:36 +0000

    refactor Home page; add values section with icons and descriptions

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,6 +1,38 @@
 /** @format */
 
+import { FaLeaf } from "react-icons/fa";
+import { IoShieldHalfSharp } from "react-icons/io5";
+import { PiPlantFill } from "react-icons/pi";
+import { FaLightbulb } from "react-icons/fa";
+
 import heroImg from "../assets/images/hero-img.jpeg";
 import sectionImg from "../assets/images/section-img.jpg";
 
-export { heroImg, sectionImg, };
+const ourValues = [
+  {
+    icons: FaLeaf,
+    title: "Quality and Health and Wellness",
+    description:
+      "Delivering fresh, organic, and high-quality products that meet the highest safety and nutritional standards, encouraging healthy living for individuals and families.",
+  },
+  {
+    icons: IoShieldHalfSharp,
+    title: "Integrity and Customer Focus",
+    description:
+      "Conducting business with honesty, transparency, and ethical practices, while putting customers at the center of every decision to ensure their needs are met with excellent service and reliable products.",
+  },
+  {
+    icons: PiPlantFill,
+    title: "Sustainability and Respect for Nature",
+    description:
+      "Commitment to eco-friendly farming practices that protect the environment, conserve resources, and preserve biodiversity while promoting long-term agricultural health.",
+  },
+  {
+    icons: FaLightbulb,
+    title: "Innovation",
+    description:
+      "Continuously seeking new methods and technologies to improve agricultural production, reduce waste, and meet changing consumer needs.",
+  },
+];
+
+export { heroImg, sectionImg, ourValues };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 /** @format */
 
 import Hero from "../components/Hero";
-import { sectionImg } from "../constants";
+import { ourValues, sectionImg } from "../constants";
 
 const Home = () => {
   return (
     <main>
       <div className="">
-        <Hero className={"h-screen"} />
-        <div className="row h-[700px]">
-          <div className="mt-[-580px] text-center justify-center">
+        <Hero className={"h-[100vh]"} />
+        <div className="row h-[650px]">
+          <div className="md:mt-[-580px] mt-[-500px] text-center justify-center">
             <div className="lg:px-[25%] md:px-10">
               <h2 className="lg:text-5xl md:text-3xl text-2xl font-bold">
                 XYZ FOODS LIMITED
@@ -19,44 +19,41 @@ const Home = () => {
                 Central Africa focusing on improving quality of life and
                 well-being through improvement in agriculture.
               </p>
-              <button className="border border-green-500 p-3 px-5 mt-2 rounded-full hover:text-white hover:bg-green-500 ">
-                Explore More
-              </button>
+              <a href="#values">
+                <button className="border border-green-600 p-3 px-5 mt-2 rounded-full hover:text-white hover:bg-green-600 ">
+                  Explore More
+                </button>
+              </a>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="flex px-[15%]">
+      <div id="values" className="flex md:px-[10%] my-[100px]">
         <img
           src={sectionImg}
           alt="section"
           width={"550px"}
-          className="hidden lg:block"
+          className="hidden xl:block rounded-lg"
         />
         <div className="w-full px-5">
-          <div className="justify-center text-center">
-            <h2>Why Choose XYZ Foods Ltd?</h2>
-            <h3>Our Values</h3>
+          <div className="justify-center text-center pb-10 pt-4">
+            <h2 className="text-4xl font-semibold pb-2">
+              Why Choose XYZ Foods Ltd?
+            </h2>
+            <h3 className="text-3xl font-semibold pt-4">Our Values</h3>
           </div>
 
           <div className="grid md:grid-cols-2 sm:grid-cols-12">
-            <div className="p-3">
-              <h3>Text</h3>
-              <p>Paragraph!</p>
-            </div>
-            <div className="p-3">
-              <h3>Text</h3>
-              <p>Paragraph!</p>
-            </div>
-            <div className="p-3">
-              <h3>Text</h3>
-              <p>Paragraph!</p>
-            </div>
-            <div className="p-3">
-              <h3>Text</h3>
-              <p>Paragraph!</p>
-            </div>
+            {ourValues.map((value) => (
+              <div key={value} className="p-8 pb-4 text-center justify-center">
+                <h2 className="text-6xl pb-3 text-green-600 flex justify-center">
+                  <value.icons />
+                </h2>
+                <h3 className="text-2xl font-semibold pb-2">{value.title}</h3>
+                <p className="text-xl">{value.description}</p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
